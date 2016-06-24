13931839ea3ac55d8fa4e5605efebd40b5b188d0
Anatoly Leskovets <ableskovets@wiley.com>
2016-06-24 18:57:27 +0300

    Day3 + remove unused files

---
+++ src/main/java/ru/aleskovets/aoc/puzzle/impl/Day3.java
@@ -0,0 +1,14 @@
+package ru.aleskovets.aoc.puzzle.impl;
+
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Component
+public class Day3 extends PuzzleImpl {
+
+    @Override
+    public void run(InputStream inputStream) throws Exception {
+
+    }
+}
---
+++ src/test/java/ru/aleskovets/aoc/puzzle/impl/Day3Test.java
@@ -0,0 +1,14 @@
+package ru.aleskovets.aoc.puzzle.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class Day3Test {
+
+    @Test
+    public void test() {
+
+    }
+}
