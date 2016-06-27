898bf5562c909d7d7dfcbb41f638cbfe1969e6aa
Anatoly Leskovets <ableskovets@wiley.com>
2016-06-27 20:49:29 +0300

    Day1 part two

--- src/main/java/ru/aleskovets/aoc/puzzle/impl/Day1.java
+++ src/main/java/ru/aleskovets/aoc/puzzle/impl/Day1.java
@@ -11,11 +11,16 @@ public class Day1 extends PuzzleImpl {
     public void run(InputStream inputStream) throws Exception {
         String input = IOUtils.toString(inputStream, "UTF-8");
         int result = 0;
+        boolean basementFound = false;
         for(int i=0; i < input.length(); i++) {
             if (input.charAt(i) == '(' ) {
                 result++;
             } else if (input.charAt(i) == ')' ) {
                 result--;
+            }
+            if(!basementFound && result == -1) {
+                logger.info("At basement: " + (i+1));
+                basementFound = true;
             }
         }
         logger.info("Result: " + result);
--- src/main/java/ru/aleskovets/aoc/puzzle/impl/Day3.java
+++ src/main/java/ru/aleskovets/aoc/puzzle/impl/Day3.java
@@ -2,13 +2,15 @@ package ru.aleskovets.aoc.puzzle.impl;
 
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Component
 public class Day3 extends PuzzleImpl {
 
     @Override
     public void run(InputStream inputStream) throws Exception {
-
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(inputStream));
     }
 }
