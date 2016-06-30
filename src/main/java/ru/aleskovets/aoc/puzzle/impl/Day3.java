1dca9ab78785be8d21354f693c879e0bf225361a
Anatoly Leskovets <ableskovets@gmail.com>
2016-07-01 00:52:03 +0300

    Day3

--- src/main/java/ru/aleskovets/aoc/puzzle/impl/Day3.java
+++ src/main/java/ru/aleskovets/aoc/puzzle/impl/Day3.java
@@ -6,12 +6,80 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @Component
 public class Day3 extends PuzzleImpl {
 
+    private int currentX = 0;
+    private int currentY = 0;
+
+    private class CoordinateChange {
+
+        CoordinateChange(String direction) {
+
+        }
+    }
+
+    private class Coordinate {
+
+        private final int x;
+        private final int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        Coordinate(String direction) {
+            switch (direction) {
+                case ">":
+                    currentX++;
+                    break;
+                case "^":
+                    currentY++;
+                    break;
+                case "<":
+                    currentX--;
+                    break;
+                case "v":
+                    currentY--;
+                    break;
+            }
+            this.x = currentX;
+            this.y = currentY;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Coordinate that = (Coordinate) o;
+
+            if (x != that.x) return false;
+            return y == that.y;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = x;
+            result = 31 * result + y;
+            return result;
+        }
+    }
+
     @Override
     public void run(Path path) throws Exception {
-        new BufferedReader(new InputStreamReader(Files.newInputStream(path)));
+        Stream<Coordinate> input = new BufferedReader(new InputStreamReader(Files.newInputStream(path)))
+                .lines()
+                .map(Coordinate::new);
+        long count = Stream.concat(Stream.of(new Coordinate(0, 0)), input)
+                .distinct()
+                .count();
+
+        logger.info("Count distinct houses: " + count);
     }
 }
