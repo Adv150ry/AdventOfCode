d52dad88acb2f6c837423093db6d49c6c44489f2
Anatoly Leskovets <ableskovets@gmail.com>
2016-07-01 01:00:40 +0300

    Day4 init

---
+++ src/main/java/ru/aleskovets/aoc/puzzle/impl/Day4.java
@@ -0,0 +1,11 @@
+package ru.aleskovets.aoc.puzzle.impl;
+
+import java.nio.file.Path;
+
+public class Day4 extends PuzzleImpl {
+
+    @Override
+    public void run(Path path) throws Exception {
+
+    }
+}
